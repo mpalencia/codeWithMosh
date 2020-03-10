71c486c47598be6d781516e7223dfbccc168c4cc
matthew_putti <matthew@puttiapps.com>
2020-03-10 17:00:07 +0800

    Lesson 3

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -11,12 +11,44 @@ import { Component } from '@angular/core';
 						{{ course }}
 					</li>
 				<ul>
+				<button class="btn btn-primary" [class.active]="isActive">Save</button>
+				<button class="btn btn-primary" [style.backgroundColor]="isActive ? 'blue': 'black'">Save</button>
+				<button (click)="onSave()">Save on click</button>
+				<button (click)="onSaveEvent($event)">Save on Event</button>
+
+				<br>
+				<input (keyup)="onKeyUp($event)" />
+				<input (keyup.enter)="onKeyUp2()" />
+				<input #email (keyup.enter)="onKeyUp3(email.value)" />
 	`				
 })
 
 export class CoursesComponent {
+	onKeyUp3(email) {
+		console.log(email);
+	}
+
+	onKeyUp2() {
+		console.log("ENTER was pressed");
+	}
+
+	onKeyUp($event) {
+		if ($event.keyCode === 13) console.log("Enter was pressed")
+	}
+
+	onSave(){
+		console.log("Button was clicked");
+	}
+
+	onSaveEvent($event){
+		console.log("Button was clicked", $event);
+	}
+
+
+    isActive = false;
 	title = "List of courses";
 	courses;
+
 
 	constructor(service: CoursesService) {
 		this.courses = service.getCourses();
